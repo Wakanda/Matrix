b6ca39837e636a219850fed3b05b7cd29bd7d824
Vincent Thibault <vthibault@vince.local>
2015-04-24 11:49:46 +0200

    Fix multiple errors
    - Properly fix dropbox property binding
    - Add child state support
    - Update using new methods API.
    - Add a message in case no datasource are bound
    - Add touch events
    - Update CSS rule rewriting to support more rules
    - Add load/draw events

--- designer.js
+++ designer.js
@@ -31,18 +31,45 @@
         description: 'On Mouse Up',
         category: 'Mouse Events'
     },{
+        name: 'touchcancel',
+        description: 'On Touch Cancel',
+        category: 'Touch Events'
+    },{
+        name: 'touchend',
+        description: 'On Touch End',
+        category: 'Touch Events'
+    },{
+        name: 'touchmove',
+        description: 'On Touch Move',
+        category: 'Touch Events'
+    },{
+        name: 'touchstart',
+        description: 'On Touch Start',
+        category: 'Touch Events'
+    },{
         name: 'rowdraw',
         description: 'On Row Draw',
-        category: 'General Events'
+        category: 'Matrix Events'
     });
+
+    // Modify properties
+    Matrix.customizeProperty('column', { description: 'Column defined either by size or number' });
+    Matrix.customizeProperty('expand', { description: 'Expand container to fit available space' });
+    Matrix.customizeProperty('rowSize', { title: 'Row size', description: 'Row size in pixels' });
+    Matrix.customizeProperty('rowMargin', { title: 'Row margin', description: 'Row margin in pixels' });
+    Matrix.customizeProperty('direction', { description: 'Scroll direction (horizontal or vertical)' });
+    Matrix.customizeProperty('columnSize', { title: 'Column size', description: 'Column size in pixels' });
+    Matrix.customizeProperty('collection', { description: 'Datasource to apply to the Matrix' });
+    Matrix.customizeProperty('columnMargin', { title: 'Column margin', description: 'Column margin in pixels' });
+    Matrix.customizeProperty('numberColumns', { title: 'Number of columns', description: 'Number of columns to display' });
 
     // Set panel menu
     Matrix.setPanelStyle({
         'fClass': true,
-        'text': true,
-        'textShadow': true,
-        'dropShadow': false,
-        'innerShadow': false,
+        'text': false,
+        'textShadow': false,
+        'dropShadow': true,
+        'innerShadow': true,
         'background': true,
         'border': true,
         'label': true,
@@ -51,14 +78,16 @@
 
 
     function updateSize() {
-        var key, num, coords, size;
+        var num, coords, size, widget;
+
+        this._recalculateSize();
 
         coords = this.repeaterGetCoordinates(0);
-        size   = this.isHorizontalScroll() ? this.width() : this.height();
+        size   = this.isHorizontalScroll() ? this._matrixWidth : this._matrixHeight;
+        widget = this.widget(0);
 
-        for (key in coords) {
-            this.widget(0)[key](coords[key]);
-        }
+        widget.node.style.width  = coords.width + 'px';
+        widget.node.style.height = coords.height + 'px';
 
         num = this.getItemsPerRow() * Math.ceil(size / this.getRowSize());
         this.studioRepeatedClones(num - 1);
@@ -68,14 +97,44 @@
     Matrix.containerChildrenAreSubWidgets();
 
     Matrix.doAfter('init', function() {
+        var errorMsg = $('<div/>');
+        errorMsg.addClass('well waf-studio-donotsave');
+        errorMsg.css({
+            display:    'none',
+            zIndex:      100,
+            position:   'absolute',
+            top:         0,
+            left:        0,
+            width:      '100%',
+            height:     '100%',
+            textAlign:  'center',
+            fontSize:   '24px',
+            padding:     10,
+            paddingTop: '30%'
+        });
+        errorMsg.text('The Collection property datasource is missing.');
+        errorMsg.appendTo(this.node);
+
         this.direction.onChange(updateSize);
         this.expand.onChange(updateSize);
         this.rowMargin.onChange(updateSize);
         this.columnMargin.onChange(updateSize);
 
-        this.number.onChange(_min('number', 1));
+        this.numberColumns.onChange(_min('numberColumns', 1));
         this.columnSize.onChange(_min('columnSize', 20));
         this.rowSize.onChange(_min('rowSize', 20));
+        this.column.onChange(showMode);
+
+        this.subscribe('datasourceBindingChange', function(event) {
+            if (event.target === 'column') {
+                showMode.call(this);
+            }
+            else if (event.target === 'collection') {
+                checkBinding.call(this);
+            }
+        }, this);
+
+        $(this.getScrolledNode()).off('touchstart, touchmove, touchend, click');
 
         // Cap value
         function _min(property, value) {
@@ -94,25 +153,34 @@
         }
 
         function showMode() {
-            if (this.mode() === 'split') {
-                this.number.show();
+            this.numberColumns.show();
+            this.columnSize.show();
+            this.expand.show();
+
+            if (this.column.boundDatasource()) {
+                // nothing to hide
+            } else if (this.column() === 'size') {
+                this.numberColumns.hide();
+            } else if (this.column() === 'number') {
                 this.columnSize.hide();
                 this.expand.hide();
-            } else {
-                this.number.hide();
-                this.columnSize.show();
-                this.expand.show();
             }
+
             updateSize.call(this);
         }
 
-        this.mode.onChange(showMode);
-        showMode.call(this);
+        function checkBinding(){
+            errorMsg.toggle(!this.collection.boundDatasource());
+        }
 
+        checkBinding.call(this);
         setTimeout(showMode.bind(this), 100);
+
+        this.repeatedWidget().addState('selected');
     });
 
     Matrix.prototype._upgradeCSSRules = function() {}; // don't mess with the stylesheets in the studio
+    Matrix.prototype._updateSize = function() {}; // deactivate runtime size update
     Matrix.prototype.studioRepeaterGetClone = (function(){
         var Container = WAF.require('Container');
         return function(position) {
--- widget.js
+++ widget.js
@@ -2,11 +2,6 @@ WAF.define('Matrix', ['waf-core/widget', 'Container'], function(widget, Containe
     'use strict';
 
     var Matrix = widget.create('Matrix', {
-        collection: widget.property({
-            type: 'datasource',
-            pageSize: 20,
-            bindable: true
-        }),
         direction: widget.property({
             type: 'enum',
             values: {
@@ -16,21 +11,26 @@ WAF.define('Matrix', ['waf-core/widget', 'Container'], function(widget, Containe
             defaultValue: 'vertical',
             bindable: true
         }),
-        mode: widget.property({
-            type: 'enum',
-            values: {
-                split: 'number of columns',
-                size: 'size'
-            },
-            defaultValue: 'split',
+        collection: widget.property({
+            type: 'datasource',
+            pageSize: 40,
             bindable: true
         }),
-        number:       widget.property({ type: 'integer', defaultValue: 2 }),
-        columnSize:   widget.property({ type: 'number',  defaultValue: 150 }),
-        columnMargin: widget.property({ type: 'number',  defaultValue: 10 }),
-        rowSize:      widget.property({ type: 'number',  defaultValue: 150 }),
-        rowMargin:    widget.property({ type: 'number',  defaultValue: 10 }),
-        expand:       widget.property({ type: 'boolean', defaultValue: true })
+        column: widget.property({
+            type: 'enum',
+            values: {
+                number: 'number of columns',
+                size: 'size'
+            },
+            defaultValue: 'number',
+            bindable: true
+        }),
+        numberColumns: widget.property({ type: 'integer', defaultValue: 2 }),
+        columnSize:    widget.property({ type: 'number',  defaultValue: 150 }),
+        columnMargin:  widget.property({ type: 'number',  defaultValue: 10 }),
+        rowSize:       widget.property({ type: 'number',  defaultValue: 150 }),
+        rowMargin:     widget.property({ type: 'number',  defaultValue: 10 }),
+        expand:        widget.property({ type: 'boolean', defaultValue: true })
     });
 
     var proto = Matrix.prototype;
@@ -40,20 +40,16 @@ WAF.define('Matrix', ['waf-core/widget', 'Container'], function(widget, Containe
     Matrix.repeaterReuseClonedWidgets();
     Matrix.repeatedWidget(Container);
 
-    function getSelectorRegExp(id) {
-        return new RegExp('#' + id + '($|[,. :\\[])');
-    }
-
     function addRules(id, newClass) {
         // Not ready yet
         if (!document.styleSheets) {
             return;
         }
 
-        var rules, styles, regexp, res;
+        var rules, styles, regexp, res, matches;
         var i, j, count, size;
 
-        regexp = getSelectorRegExp(id);
+        regexp = new RegExp('#' + id + '($|[,. :\\[].*)');
         styles = document.styleSheets;
         count  = styles.length;
         res    = [];
@@ -63,8 +59,8 @@ WAF.define('Matrix', ['waf-core/widget', 'Container'], function(widget, Containe
             size  = rules.length;
 
             for (j = 0; j < size; j++) {
-                if (regexp.test(rules[j].selectorText)) {
-                    styles[i].insertRule('.' + newClass + '{' + rules[j].style.cssText + '}', 0);
+                if (rules[j].selectorText && (matches = rules[j].selectorText.match(regexp))) {
+                    styles[i].insertRule('.waf-theme:not(.vip) .' + newClass + matches[1] + ' {' + rules[j].style.cssText + '}', styles[i].cssRules.length);
                 }
             }
         }
@@ -76,18 +72,51 @@ WAF.define('Matrix', ['waf-core/widget', 'Container'], function(widget, Containe
         widget.addClass(newClass);
     }
 
+
+    // Avoid read/write operation in rendering
+    proto._recalculateSize = function (fixHiddenMatrix) {
+        var $node = $(this.node.firstChild);
+        var rendered = !!$node.parents(':visible').length;
+
+        // Can't get clientWidth and clientHeight when the matrix is invisible
+        // Have to show all hidden parents element, get the position, and hide them again
+        if (rendered) {
+            var parents = $(null);
+            var parent;
+
+            while ($node.is(':hidden')) {
+                parent = $node.parents(':hidden').last();
+                if (!parent.length) {
+                    break;
+                }
+                parent.show();
+                parents.push(parent);
+            }
+
+            parent = $node.parent();
+            this._matrixWidth  = parent.width();
+            this._matrixHeight = parent.height();
+
+            parents.hide();
+        }
+    };
+
     proto._getColumnsAvailableSize = function() {
-        var size = this.isHorizontalScroll() ? this.node.clientHeight : this.node.clientWidth;
+        if (!this._matrixWidth || !this._matrixHeight) {
+            this._recalculateSize();
+        }
+
+        var size = this.isHorizontalScroll() ? this._matrixHeight : this._matrixWidth;
         return size + this.columnMargin() - 20; // TOFIX: hardcoded scrollbar size
     };
 
     proto._getColumnSize = function() {
-        if (this.mode() === 'split') {
-            return (this._getColumnsAvailableSize() - this.number() * this.columnMargin()) / this.number();
+        if (this.column() === 'number') {
+            return Math.floor((this._getColumnsAvailableSize() - this.numberColumns() * this.columnMargin()) / this.numberColumns() );
         }
 
         if (this.expand()) {
-            return this._getColumnsAvailableSize() / Math.floor(this._getColumnsAvailableSize() / (this.columnSize() + this.columnMargin())) - this.columnMargin();
+            return Math.floor(this._getColumnsAvailableSize() / Math.floor(this._getColumnsAvailableSize() / (this.columnSize() + this.columnMargin())) - this.columnMargin());
         }
 
         return this.columnSize();
@@ -142,8 +171,8 @@ WAF.define('Matrix', ['waf-core/widget', 'Container'], function(widget, Containe
     };
 
     proto.getItemsPerRow = function() {
-        if (this.mode() === 'split') {
-            return this.number();
+        if (this.column() === 'number') {
+            return this.numberColumns();
         }
 
         return Math.floor(this._getColumnsAvailableSize() / (this.columnSize() + this.columnMargin()));
@@ -154,17 +183,48 @@ WAF.define('Matrix', ['waf-core/widget', 'Container'], function(widget, Containe
     };
 
     proto.getThreshold = function() {
-        var size = this.isHorizontalScroll() ? this.node.clientWidth : this.node.clientHeight;
-        return Math.ceil(size / this.getRowSize() / 2) * this.getItemsPerRow();
+        if (!this._matrixWidth || !this._matrixHeight) {
+            this._recalculateSize();
+        }
+
+        var size = this.isHorizontalScroll() ? this._matrixWidth : this._matrixHeight;
+        return Math.ceil(0.2 * size / this.getRowSize()) * this.getItemsPerRow();
     };
 
     proto.init = function() {
+        var _target = null;
         var scrolled = this.getScrolledNode();
+        var $scrolled = $(scrolled);
+        var $scroller = $(this.getScrollerNode());
 
         this._upgradeCSSRules();
         this.addClass('waf-matrix2');
 
-        $(scrolled).on('click', function(event) {
+        // Mobile touch optimization
+        $scrolled.on('touchstart', function(event) {
+            _target = event.target;
+        });
+
+        // Unselect target
+        $scrolled.on('touchmove', function(){
+            _target = null;
+        });
+
+        // Overwrite onPrefetch to recalculate matrix size and avoid read/write
+        // operation during rendering.
+        var onPrefetch = this.onPrefetch;
+        this.onPrefetch = function() { 
+            this._recalculateSize();
+            onPrefetch.apply(this, arguments);
+        };
+
+        // Change element
+        $scrolled.on('click touchend', function(event) {
+            // Avoid selecting a node on touchscroll
+            if (event.type === 'touchend' && (!_target || event.target !== _target)) {
+                return false;
+            }
+
             var collection = this.collection();
             var target     = event.target;
 
@@ -190,14 +250,52 @@ WAF.define('Matrix', ['waf-core/widget', 'Container'], function(widget, Containe
             var scrolling = {};
             var key = 'scroll' + (this.isHorizontalScroll() ? 'Left' : 'Top');
             var pos = Math.max(0, this._getTop(position) - 40);
-            var scroller = $(scrolled).parent();
             var size = this.isHorizontalScroll() ? this.width() : this.height();
 
-            if (pos < scroller[key]() || pos > scroller[key]() + size) {
+            if (pos < $scroller[key]() || pos > $scroller[key]() + size) {
                 scrolling[key] = pos;
-                $(scrolled).parent().animate(scrolling, 500);
+                $scroller.animate(scrolling, 500);
             }
         }, this);
+
+        // Reset scrollbar
+        this.collection.onChange(function(){
+            $scroller['scroll' + (this.isHorizontalScroll() ? 'Left' : 'Top')](0);
+        });
+
+        this.subscribe('rowload', function(event){
+            event.data.widget.addClass('waf-state-loading');
+        });
+
+        this.subscribe('rowdraw', function(event){
+            event.data.widget.removeClass('waf-state-loading');
+        });
+
+        this.direction.onChange(this._updateSize);
+        this.expand.onChange(this._updateSize);
+        this.rowMargin.onChange(this._updateSize);
+        this.columnMargin.onChange(this._updateSize);
+        this.numberColumns.onChange(this._updateSize);
+        this.columnSize.onChange(this._updateSize);
+        this.rowSize.onChange(this._updateSize);
+    };
+
+    proto._updateSize = function() {
+        this.getScrollerNode().scrollLeft = 0;
+        this.getScrollerNode().scrollTop = 0;
+        if(this.collection.start() !== 0) {
+            this.collection.fetch(0);
+        } else {
+            this.widgets().forEach(function(widget) {
+                var position = this.getPosition(widget);
+                var coords = this.repeaterGetCoordinates(position);
+                for(var key in coords) {
+                    if(widget[key]) {
+                        widget[key](coords[key]);
+                    }
+                }
+            }.bind(this));
+        }
     };
 
     return Matrix;
